485e706aea31e51dc0fb62fa233d480bc0cb7e04
Luis Garrillo <garrillofeeling@gmail.com>
2025-05-18 20:38:02 +0000

    Implement upper walls

--- frame-breakers/scripts/level/holders/walls_holder.gd
+++ frame-breakers/scripts/level/holders/walls_holder.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name WallsHolder
 @export var wallA : Wall
 @export var wallB : Wall
-
+const UPPER_WALLS = preload("res://scenes/level/upper_walls.tscn")
 const WALLS = preload("res://scenes/level/walls.tscn")
 
 func _ready() -> void:
@@ -12,10 +12,11 @@ func _ready() -> void:
 func set_up_wall(wall : Wall) -> void:
 	wall.wall_switch.connect(switch_walls)
 	
-func switch_walls() -> void:
+func switch_walls(side) -> void:
 	remove_child(wallA)
 	wallA = wallB
-	wallB = WALLS.instantiate()
+	wallB = WALLS.instantiate() if side == "lower" else UPPER_WALLS.instantiate()
 	add_child(wallB)
 	set_up_wall(wallB)
+
 	wallB.position.x = Global.screen_width / 2
