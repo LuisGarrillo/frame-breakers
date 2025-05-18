bbb591195ba8cbc70364320a13f3acd12b4027f0
Luis Garrillo <garrillofeeling@gmail.com>
2025-05-18 18:55:39 +0000

    Add projectile deleting

--- frame-breakers/scripts/level/projectile.gd
+++ frame-breakers/scripts/level/projectile.gd
@@ -1,14 +1,18 @@
 extends Area2D
 class_name Projectile
 
+@export var limit : int = 960
 var masks_map: Dictionary[String, int] = {
 	"Ally": 1,
 	"Enemy": 0
 }
 var direction
 
+
 func _physics_process(delta: float) -> void:
 	position.x += Global.speed * delta * direction
+	if position.x < limit * -1 or position.x > limit:
+		queue_free()
 
 func set_up(mode, setup_direction) -> void:
 	set_collision_mask_value(masks_map[mode], true)
