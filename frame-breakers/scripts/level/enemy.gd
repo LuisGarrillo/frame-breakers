fa54cb948cb02a7e3f2491aa3b6d3d18feb2ed30
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-05-24 20:13:10 -0400

    Implement enemy

---
+++ frame-breakers/scripts/level/enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+class_name Enemy
+@onready var shoot_timer: Timer = $ShootTimer
+
+@export var shoot_cooldown : float
+
+signal shoot
+signal death
+
+var carril : int
+var enter_limit = (Global.screen_width / 2) - 48
+
+func _ready() -> void:
+	shoot_timer.wait_time = shoot_cooldown
+
+func _process(delta: float) -> void:
+	enter(delta)
+
+func enter(delta) -> void:
+	if position.x >= enter_limit:
+		position.x -= Global.speed * delta * 2
+
+func die():
+	death.emit(carril)
+	queue_free()
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area is Projectile and area.type == "Ally":
+		die()
+
+func _on_shoot_timer_timeout() -> void:
+	shoot.emit(carril)
