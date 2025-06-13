8531e4ffd47a442295e98fbf122e66a98eb66628
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-06-12 21:15:04 -0400

    Finish game

--- frame-breakers/scripts/level/level.gd
+++ frame-breakers/scripts/level/level.gd
@@ -7,16 +7,22 @@ class_name Level
 @onready var enemies_holder: Node2D = $EnemiesHolder
 @onready var walls_holder: Node2D = $WallsHolderA
 @onready var enemy_generation: Timer = $EnemyGeneration
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var tutorial_timer: Timer = $TutorialTimer
 
 @export var time_ratio : int = 30
 @export var speed_change_ratio : float = 1.2
 
 var active : bool = false
+var locked : bool = true
+var on_game_over : bool = false
 var frame_accumulator : float = 0
 var accumulator := 0
 var increment : int
 var carrils = [0, 1 ,2]
 var kill_count : int = 0
+
+
 @export var enemy_wait_shoot_decreaser : float = 0.25
 @export var enemy_wait_shoot : float = 1
 
@@ -30,8 +36,12 @@ const PLAYER_PROJECTILE = preload("res://scenes/level/projectiles/player_project
 const ENEMY_PROJECTILE = preload("res://scenes/level/projectiles/enemy_projectile.tscn")
 const ENEMY = preload("res://scenes/level/enemy.tscn")
 
+func _ready() -> void:
+	$Sound/in.play()
+	animation_player.play("in Title")
+
 func _physics_process(delta: float) -> void:
-	if not active:
+	if not active or locked:
 		return
 	
 	if increment == 5:
@@ -49,22 +59,44 @@ func _physics_process(delta: float) -> void:
 		Global.speed *= speed_change_ratio
 
 func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Close"):
+		get_tree().quit()
 	
+	if active:
+		return
+	
+	if not on_game_over:
+		title_input()
+	else:
+		game_over_input()
+
+
+func title_input() -> void:
+	if Input.is_action_just_pressed("accept"):
+		$Sound/accept.play()
+		active = true
+		animation_player.play("out Title")
+		
+func game_over_input() -> void:
+	if Input.is_action_just_pressed("accept"):
+		$Sound/accept.play()
+		active = true
+		animation_player.play("out Over")
 
 func check_increment() -> void:
 	if increment == 2:
-		enemy_generation.wait_time = 3.5
+		enemy_generation.wait_time = 3.0
 	elif increment == 3:
 		enemy_generation.wait_time = 1.5
 		enemy_wait_shoot -= enemy_wait_shoot_decreaser
 	elif increment == 5:
-		enemy_generation.wait_time = 0.9
+		enemy_generation.wait_time = 0.75
 		enemy_wait_shoot -= enemy_wait_shoot_decreaser
 
 func generate_enemy():
 	if not carrils:
 		return
-	
+	$"Sound/enemy-in".play()
 	var new_enemy : Enemy = ENEMY.instantiate()
 	var index = randi_range(0, len(carrils) - 1)
 	var carril = carrils.pop_at(index)
@@ -79,14 +111,20 @@ func generate_enemy():
 func enemy_death(carril):
 	carrils.append(carril)
 	kill_count += 1
+	update_score_count()
+
+func update_score_count() -> void:
+	return
 
 func add_enemy_projectile(carril) -> void:
+	$Sound/shoot.play()
 	var new_projectile = ENEMY_PROJECTILE.instantiate()
 	projectiles_holder.add_child(new_projectile)
 	new_projectile.position = Vector2((Global.screen_width / 2) - 48, carril_map[carril].y)
 	new_projectile.set_up(-1, "Enemy")
 
 func _on_player_add_projectile() -> void:
+	$Sound/shoot.play()
 	var new_projectile = PLAYER_PROJECTILE.instantiate()
 	projectiles_holder.add_child(new_projectile)
 	new_projectile.position = player.position
@@ -95,3 +133,47 @@ func _on_player_add_projectile() -> void:
 
 func _on_enemy_generation_timeout() -> void:
 	generate_enemy()
+
+func start_player() -> void:
+	player.is_started = true
+
+func unlock() -> void:
+	locked = false
+
+func free_enemies() -> void:
+	for enemy: Enemy in enemies_holder.get_children():
+		enemy.animation_player.play("disappear")
+
+func reset() -> void:
+	player.visible = true
+	player.position = Vector2(-288, 0)
+	player.set_collision_layer_value(1, true)
+	enemy_generation.wait_time = 4.0
+	increment = 1
+	Global.speed = 175
+	start_player()
+
+func _on_player_start_finish() -> void:
+	if on_game_over:
+		enemy_generation.start()
+		on_game_over = false
+		return
+	animation_player.play("in Tutorial")
+
+
+func _on_tutorial_timer_timeout() -> void:
+	animation_player.play("out Tutorial")
+	enemy_generation.start()
+	
+
+
+func _on_player_sig_die() -> void:
+	if on_game_over:
+		return
+		
+	free_enemies()
+	locked = true
+	active = false
+	enemy_generation.stop()
+	on_game_over = true
+	animation_player.play("in Over")
